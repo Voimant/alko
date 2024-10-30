37d1e4ac43a6773a80715bf3043662111b7398a2
Voimant <voimant007@gmail.com>
2024-10-30 07:20:36 +0500

    fix neiro

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def main():
     dp = Dispatcher()
     logging.basicConfig(level=logging.INFO)
     dp.include_routers(handlers.main_handlers.router,)
-    aiocron.crontab('08 09 * * *', func=get_mem_day, start=True)
+    aiocron.crontab('05 30 * * *', func=get_mem_day, start=True)
     await dp.start_polling(bot)
 
 
--- handlers/func_mem.py
+++ handlers/func_mem.py
@@ -9,6 +9,8 @@ from aiogram.types import FSInputFile
 from dotenv import load_dotenv
 from aiogram import Bot
 
+from wiki import update_token, neiro
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
@@ -16,10 +18,7 @@ PIC_PATH = os.getenv('PIC_PACH')
 
 
 async def get_mem_day():
-    list_mems = os.listdir(f'/root{PIC_PATH}')
-    x = random.randint(0, len(list_mems))
-    mem_day = list_mems[x]
-    file = FSInputFile(f'/root{PIC_PATH}/{str(mem_day)}')
-    await bot.send_photo(chat_id=-1001818033439, photo=file, caption=f'Всем привет! Мем дня!')
-    await bot.send_message(-1001818033439, '!')
+    update_token()
+    response = neiro('оригинально пожелай друзьям доброго утра и в конце напиши "ОЖИДАЕМ БУСЮ ДНЯ!"')
+    await bot.send_message(-1001818033439, f'{response}')
 
